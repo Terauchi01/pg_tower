c5c2445d8d8adfadf26a978ef5e09da2342c51e6
LAPTOP-5J71A4JM\enwok <enwokakuhito2@gmail.com>
2023-03-06 15:09:38 +0900

    城追加

--- kubota/main.js
+++ kubota/main.js
@@ -5,24 +5,25 @@ canvas.height = document.documentElement.scrollHeight;
 const ctx = canvas.getContext("2d");
 
 // 画像の読み込み
-const imgPaths = ["Image/soldier.png", "Image/lancer.png", "Image/cavalry.png"];
+const imgPaths = ["Image/soldier.png", "Image/lancer.png", "Image/cavalry.png", "Image/castle.png"];
 const images = [];
 
 const player = new Player(1);
 
 const lanePosx = [canvas.width / 4, canvas.width / 2, canvas.width / 4 * 3];
 
-player.addUnit(0, new Soldier(lanePosx[0], 400, 10, 1, 4));
-player.addUnit(1, new Cavalry(lanePosx[1], 500, 10, 1, 5));
-player.addUnit(2, new Cavalry(lanePosx[2], 400, 10, 1, 6));
+player.addUnit(0, new Soldier(lanePosx[0], 400, 1, 4));
+player.addUnit(1, new Cavalry(lanePosx[1], 500, 1, 5));
+player.addUnit(2, new Cavalry(lanePosx[2], 400, 1, 6));
 
-player.addUnit(0, new Soldier(lanePosx[0], canvas.height, 10, 1, 1));
-player.addUnit(1, new Lancer(lanePosx[1], canvas.height, 10, 1, 2));
-player.addUnit(2, new Cavalry(lanePosx[2], canvas.height, 10, 1, 3));
+player.addUnit(0, new Soldier(lanePosx[0], canvas.height, 1, 1));
+player.addUnit(1, new Lancer(lanePosx[1], canvas.height, 1, 2));
+player.addUnit(2, new Cavalry(lanePosx[2], canvas.height, 1, 3));
+player.addUnit(2, new Cavalry(lanePosx[2], canvas.height, 1, 3));
 
 const enemy = new Player(2);
-enemy.addUnit(0, new Soldier(lanePosx[2], canvas.height, 10, 2, 7));
-enemy.addUnit(1, new Lancer(lanePosx[1], canvas.height, 10, 2, 8));
+enemy.addUnit(0, new Soldier(lanePosx[2], canvas.height, 2, 7));
+enemy.addUnit(1, new Lancer(lanePosx[1], canvas.height, 2, 8));
 
 Promise.all(imgPaths.map(path => {
   return new Promise((resolve, reject) => {
@@ -39,10 +40,10 @@ Promise.all(imgPaths.map(path => {
 })).then(() => {
 
   function drawUnit(playerObj) {
-
     for (ary of playerObj.lanes) {
       for (obj of ary) {
         let px = obj.pos[0], py = obj.pos[1];
+        px = px - images[0].width/2
         if (playerObj.playerID != 1) {
           py = canvas.height - py;
         }
@@ -102,6 +103,8 @@ Promise.all(imgPaths.map(path => {
     // 最初に1回だけclearRectを呼ぶ
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     //x, yを更新することで画像の座標を変更できる
+    ctx.drawImage(images[3], canvas.width/2-images[3].width/2, 0);
+    ctx.drawImage(images[3], canvas.width/2-images[3].width/2, canvas.height-images[3].height);
     drawUnit(player);
     drawUnit(enemy);
   }
