faff6a87b14f3c1d4bef6fa7b7b2d403efa3fa14
LAPTOP-5J71A4JM\enwok <enwokakuhito2@gmail.com>
2023-03-06 14:26:46 +0900

    mainの中身を関数化

--- kubota/main.js
+++ kubota/main.js
@@ -8,7 +8,7 @@ const ctx = canvas.getContext("2d");
 const imgPaths = ["Image/soldier.png", "Image/lancer.png", "Image/cavalry.png"];
 const images = [];
 
-const player = new Player();
+const player = new Player(1);
 
 const lanePosx = [canvas.width / 4, canvas.width / 2, canvas.width / 4 * 3];
 
@@ -20,7 +20,7 @@ player.addUnit(0, new Soldier(lanePosx[0], canvas.height, 10, 1, 1));
 player.addUnit(1, new Lancer(lanePosx[1], canvas.height, 10, 1, 2));
 player.addUnit(2, new Cavalry(lanePosx[2], canvas.height, 10, 1, 3));
 
-const enemy = new Player();
+const enemy = new Player(2);
 enemy.addUnit(0, new Soldier(lanePosx[2], canvas.height, 10, 2, 7));
 enemy.addUnit(1, new Lancer(lanePosx[1], canvas.height, 10, 2, 8));
 
@@ -37,88 +37,83 @@ Promise.all(imgPaths.map(path => {
     images.push(img);
   });
 })).then(() => {
-  // 画像を描画する関数
-  function drawImage() {
-    // 最初に1回だけclearRectを呼ぶ
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //x, yを更新することで画像の座標を変更できる
 
-    for (ary of player.lanes) {
+  function drawUnit(playerObj) {
+
+    for (ary of playerObj.lanes) {
       for (obj of ary) {
-        switch (obj.constructor) {
-          case Soldier:
-            ctx.drawImage(images[0], obj.pos[0], obj.pos[1]);
-            break;
-          case Lancer:
-            ctx.drawImage(images[1], obj.pos[0], obj.pos[1]);
-            break;
-          case Cavalry:
-            ctx.drawImage(images[2], obj.pos[0], obj.pos[1]);
-            break;
+        let px = obj.pos[0], py = obj.pos[1];
+        if (playerObj.playerID != 1) {
+          py = canvas.height - py;
         }
-      }
-    }
-
-    for (ary of enemy.lanes) {
-      for (obj of ary) {
         switch (obj.constructor) {
           case Soldier:
-            ctx.drawImage(images[0], obj.pos[0], canvas.height - obj.pos[1]);
+            ctx.drawImage(images[0], px, py);
             break;
           case Lancer:
-            ctx.drawImage(images[1], obj.pos[0], canvas.height - obj.pos[1]);
+            ctx.drawImage(images[1], px, py);
             break;
           case Cavalry:
-            ctx.drawImage(images[2], obj.pos[0], canvas.height - obj.pos[1]);
+            ctx.drawImage(images[2], px, py);
             break;
         }
       }
     }
   }
 
+  function attackUnit(playerObj1, playerObj2) {
+    for (let i = 0; i < playerObj1.lanes.length; i++) {
+      if (playerObj1.lanes[i].length == 0 || playerObj2.lanes[playerObj2.lanes.length - i - 1].length == 0) continue;
+      if (Math.abs(playerObj1.lanes[i][0].pos[1] - (canvas.height - playerObj2.lanes[playerObj2.lanes.length - i - 1][0].pos[1])) <= images[0].height) {
+        playerObj1.lanes[i][0].isMove = false;
+        playerObj2.lanes[playerObj2.lanes.length - i - 1][0].isMove = false;
+        playerObj1.lanes[i][0].attack(playerObj2.lanes[playerObj2.lanes.length - i - 1][0]);
+        playerObj2.lanes[playerObj2.lanes.length - i - 1][0].attack(playerObj1.lanes[i][0]);
+      }
+    }
+  }
+
+  function updateUnit(playerObj1, playerObj2) {
+    for (let i = 0; i < playerObj1.lanes.length; i++) {
+      for (let j = 0; j < playerObj1.lanes[i].length; j++) {
+        if (j == 0) {
+          if (playerObj2.lanes[playerObj2.lanes.length - i - 1].length == 0 ||
+            Math.abs(playerObj1.lanes[i][j].pos[1] - (canvas.height - playerObj2.lanes[playerObj2.lanes.length - i - 1][0].pos[1])) > images[0].height) {
+            playerObj1.lanes[i][j].isMove = true;
+          }
+        } else {
+          if (Math.abs(playerObj1.lanes[i][j - 1].pos[1] - playerObj1.lanes[i][j].pos[1]) > images[0].height) {
+            playerObj1.lanes[i][j].isMove = true;
+          } else {
+            if (playerObj1.lanes[i][j - 1].constructor == playerObj1.lanes[i][j].constructor) {
+              playerObj1.lanes[i][j - 1].isCooperate = true;
+            }
+            playerObj1.lanes[i][j].isMove = false;
+          }
+        }
+        playerObj1.lanes[i][j].update();
+        if (playerObj1.lanes[i][j].hp <= 0) playerObj1.eraseUnit(i);
+      }
+    }
+  }
+
+  // 画像を描画する関数
+  function drawImage() {
+    // 最初に1回だけclearRectを呼ぶ
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //x, yを更新することで画像の座標を変更できる
+    drawUnit(player);
+    drawUnit(enemy);
+  }
+
   function mainLoop() {
     //ここに繰り返したいものを書く
     //attack
-    for (let i = 0; i < player.lanes.length; i++) {
-      if (player.lanes[i].length == 0 || enemy.lanes[enemy.lanes.length - i - 1].length == 0) continue;
-      if (Math.abs(player.lanes[i][0].pos[1] - (canvas.height - enemy.lanes[enemy.lanes.length - i - 1][0].pos[1])) <= images[0].height) {
-        player.lanes[i][0].isMove = false;
-        enemy.lanes[enemy.lanes.length - i - 1][0].isMove = false;
-        player.lanes[i][0].attack(enemy.lanes[enemy.lanes.length - i - 1][0]);
-        enemy.lanes[enemy.lanes.length - i - 1][0].attack(player.lanes[i][0]);
-      }
-    }
+    attackUnit(player, enemy);
 
     //move
-    for (let i = 0; i < player.lanes.length; i++) {
-      for (let j = 0; j < player.lanes[i].length; j++) {
-        if (j == 0) {
-          if (enemy.lanes[enemy.lanes.length - i - 1].length == 0 ||
-            Math.abs(player.lanes[i][j].pos[1] - (canvas.height - enemy.lanes[enemy.lanes.length - i - 1][0].pos[1])) > images[0].height) {
-            player.lanes[i][j].isMove = true;
-          }
-        } else {
-          player.lanes[i][j].isMove = Math.abs(player.lanes[i][j - 1].pos[1] - player.lanes[i][j].pos[1]) > images[0].height;
-        }
-        player.lanes[i][j].update();
-        if (player.lanes[i][j].hp <= 0) player.eraseUnit(i);
-      }
-    }
-
-    for (let i = 0; i < enemy.lanes.length; i++) {
-      for (let j = 0; j < enemy.lanes[i].length; j++) {
-        if (j == 0) {
-          if (player.lanes[player.lanes.length - i - 1].length == 0 ||
-            Math.abs(enemy.lanes[i][j].pos[1] - (canvas.height - player.lanes[player.lanes.length - i - 1][0].pos[1])) > images[0].height) {
-            enemy.lanes[i][j].isMove = true;
-          }
-        } else {
-          enemy.lanes[i][j].isMove = Math.abs(enemy.lanes[i][j - 1].pos[1] - enemy.lanes[i][j].pos[1]) > images[0].height;
-        }
-        enemy.lanes[i][j].update();
-        if (enemy.lanes[i][j].hp <= 0) enemy.eraseUnit(i);
-      }
-    }
+    updateUnit(player, enemy);
+    updateUnit(enemy, player);
 
     drawImage();
     requestAnimationFrame(mainLoop);
